2d2da352c64121c64b72c76148d38243a3a0d6ba
Robson Maia <robsonmvieira@gmail.com>
2022-01-08 15:46:43 -0300

    ✨ add new mapper method

--- src/mappers/supplier.mapper.ts
+++ src/mappers/supplier.mapper.ts
@@ -1,14 +1,28 @@
+import { Address } from "../models/address.model";
 import { IMapper } from "../models/imapper";
 import { Supplier } from "../models/supplier.model";
 
 export class SupplierMapper implements IMapper<Supplier> {
   map(item: any): Supplier {
-    return new Supplier(item)
+    const supplier = new Supplier(item)
+    return supplier
   }
   unmap(item: Supplier): object {
     return {
       ...item
     }
   }
+
+  addressMapper(data: any): Address {
+    return {
+    street: data.logradouro,
+    number: '',
+    zipCode: data.cep,
+    city: data.localidade,
+    neighborhood: data.bairro,
+    state: data.uf,
+    moreInfo: data.complemento
+  }
+}
 
 }
